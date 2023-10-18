d5f3ab72dfbd2f260ae84c5185dc4b24ec0a9e79
Manas-Jorvekar <148330415+Manas-Jorvekar@users.noreply.github.com>
2023-10-18 15:20:25 +0530

    Update header.h

--- header.h
+++ header.h
@@ -412,3 +412,26 @@ void Schedule_new_lecture(char *faculty_name, char *class_name, char *subject) {
     insertClass(new_class);
 }
 
+void displayDayTimetable_for_student(){
+    char class_name[20];char weekday[20];
+    printf("Enter you classname among the following:\n1)SY_CE1\n2)SY_CE2\n3)TY_CE1\n4)TY_CE2\n5)BT_CE1\n6)BT_CE2\n7)FY_MT\n");
+    scanf("%s",class_name);
+    printf("Which day's schedule you wish to see:   ");
+    scanf("%s",weekday);
+    int i = 0;
+    while(i<5){
+        ll_Node* temp = timetable[i];
+        if(!strcmp(weekday,temp->data.day)) 
+            break;
+        i++;
+    }
+    
+    ll_Node *temp = timetable[i];
+        if (timetable[i] == NULL) printf("No Schedule");
+        else {
+            while (temp != NULL) {
+                if (!strcmp(temp->data.class_name, class_name)) printf("%s : \n Time : %s \n Subject : %s Faculty : %s \n \n Classroom : %s \n", temp->data.day, temp->data.time, temp->data.subject, temp->data.faculty, temp->data.classroom);
+                temp = temp->next;
+            }
+        }
+}
